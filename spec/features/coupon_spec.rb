2c1fbc0043eebbc0d2c7e998a290c9bc19bcc8a2
Robert Groller <robertgroller@gmail.com>
2014-09-25 17:50:44 -0400

    Add feature specs

---
+++ spec/features/coupon_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Coupon do
+  it "allows sellers to create a coupon with either a number or percentage"
+  it "sends shoppers an email with the coupon info"
+  it "allows shoppers to apply the coupon to an item at checkout time"
+end
